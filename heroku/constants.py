2fa1fda187c65aa6aa8ae9bb666aea555ddf1a17
viyin <viyin@cisco.com>
2016-09-26 13:23:37 -0400

    API token

--- heroku/constants.py
+++ heroku/constants.py
@@ -16,7 +16,7 @@ ITTY = {
 ##
 # Authorization token for Spark. Required for working with Spark's API.
 #
-AUTH = "Put your token here"
+AUTH = "YjFhNzY0NTktZjhiOC00NTM3LWE5OGQtNmMwN2NjZTI5ZjE0Y2QyOGEwMjItNWVl"
 
 ##
 # Request header for Spark Web API
